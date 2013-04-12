4e335202c57a20aeb21f8b39fefe221be3b9538e
Vulcain <vulcain@shopelia.fr>
2013-04-12 13:20:42 +0200

    hot fix

--- lib/core/worker.rb
+++ lib/core/worker.rb
@@ -18,7 +18,7 @@ module Vulcain
           rescue => e
             puts e.inspect
             puts e.backtrace.join("\n")
-            puts state_machine.strategy.driver.page_source
+            puts state_machine.strategy.driver.driver.page_source
             # log
             exchanger = Vulcain::Exchanger.new(message['session'])
             message = {'verb' => 'failure'}
