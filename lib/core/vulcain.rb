b99119d1b68ae57c80ebdbb4fef9d997cf2731ea
anoiaque <anoiaque@gmail.com>
2013-04-19 16:46:24 +0200

    Configuration of AMQP servers via config/vulcain.yml

--- lib/core/vulcain.rb
+++ lib/core/vulcain.rb
@@ -2,23 +2,19 @@
 require "amqp"
 require "json"
 require "selenium-webdriver"
+require "yaml"
 
 module Vulcain
-  DISPATCHER_HOST = "127.0.0.1"
-  DISPATCHER_USER = "guest"
-  DISPATCHER_PASSWORD = "guest"
   DISPATCHER_VULCAINS_QUEUE = "vulcains-queue" #DO NOT CHANGE WITHOUT CHANGE ON VULCAIN-API
-  LOGGING_QUEUE = "logging-queue" #DO NOT CHANGE WITHOUT CHANGE ON VULCAIN
+  LOGGING_QUEUE = "logging-queue" #DO NOT CHANGE WITHOUT CHANGE ON VULCAIN-API
   VULCAIN_QUEUE = lambda { |vulcain_id| "vulcain-#{vulcain_id}" }
-  USER = "guest"
-  PASSWORD = "guest"
-  HOST = "127.0.0.1"
-  
+  CONFIG = YAML.load_file File.join(File.dirname(__FILE__), '../../config/vulcain.yml')
   @@exchanger = nil
   
   def exchanger
     return @@exchanger if @@exchanger
-    connection = AMQP::Session.connect(host:DISPATCHER_HOST, username:DISPATCHER_USER, password:DISPATCHER_PASSWORD)
+    config = CONFIG['dispatcher']
+    connection = AMQP::Session.connect(host:config['host'], username:config['user'], password:config['password'])
     channel = AMQP::Channel.new(connection)
     channel.on_error do |channel, channel_close|
       raise "Can't open channel to dispatcher MQ on #{DISPATCHER_HOST}"
