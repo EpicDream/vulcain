becc05fc16ba828d40cd48a82858c81b9e7caf81
anoiaque <anoiaque@gmail.com>
2013-04-26 14:26:57 +0200

    clean

--- lib/core/state_machine.rb
+++ lib/core/state_machine.rb
@@ -1,12 +1,9 @@
 module Vulcain
   class StateMachine
     
-    attr_reader :robot#, :session
+    attr_reader :robot
     
-    def initialize #exchange, id
-      # @exchange = exchange
-      # @id = id
-      # @admin_exchanger = Vulcain::AdminExchanger.new({vulcain_id:@id})
+    def initialize
       @messager = Vulcain.messager
     end
     
@@ -14,20 +11,15 @@ module Vulcain
       @messager.session = message['context']['session']
       @robot = Object.const_get(message['vendor']).new(message['context']).robot
       @robot.messager = @messager
-      # @robot.exchanger = Vulcain::DispatcherExchanger.new(session)
-      # @robot.self_exchanger = Vulcain::SelfExchanger.new(session, @exchange)
-      # @robot.logging_exchanger = Vulcain::LoggingExchanger.new(session)
     end
     
     def handle message
       case message['verb']
       when MESSAGES_VERBS[:ping]
         @messager.admin.message(:ack_ping)
-        # @admin_exchanger.publish({status:ADMIN_MESSAGES_STATUSES[:ack_ping]})
       when MESSAGES_VERBS[:reload]
         Vulcain.reload(message['code'])
         @messager.admin.message(:reloaded)
-        # @admin_exchanger.publish({status:ADMIN_MESSAGES_STATUSES[:reloaded]})
         $stdout << "Ouch ! My code has been hot reloaded. Ready !\n"
       when 'answer'
         @robot.context = message['context']
--- lib/core/worker.rb
+++ lib/core/worker.rb
@@ -14,7 +14,6 @@ module Vulcain
         $stdout << "I'm Vulcain number #{@id} and i'm started !\n"
         @messager.session = { vulcain_id:@id }
         @messager.admin.message(:started)
-        # Vulcain::AdminExchanger.new({vulcain_id:@id}).publish({:status => ADMIN_MESSAGES_STATUSES[:started]})
         
         channel.queue.bind(exchange, :arguments => { 'x-match' => 'all', :queue => VULCAIN_QUEUE.(@id)}).subscribe do |metadata, message|
           begin
@@ -25,24 +24,16 @@ module Vulcain
             if session
               @messager.session = session
               @messager.dispatcher.message(:failure, { status:'exception'})
-              #message = {'verb' => MESSAGES_VERBS[:failure], 'content' => {status:'exception'}}
-              # DispatcherExchanger.new(session).publish(message)
             end
-            #exchanger = LoggingExchanger.new(session)
             if @state_machine && @state_machine.robot
               driver = @state_machine.robot.driver
               @message.logging.message(:screenshot, driver.screenshot)
               @message.logging.message(:page_source, driver.page_source)
-              # exchanger.publish({ screenshot:driver.screenshot })
-              # exchanger.publish({ page_source:driver.page_source })
               driver.quit
             end
             @messager.admin.message(:failure)
             @message.logging.message(:error_message, e.inspect)
             @message.logging.message(:stack_trace, e.backtrace.join("\n"))
-            # AdminExchanger.new({vulcain_id:@id}).publish({status:ADMIN_MESSAGES_STATUSES[:failure]})
-            # exchanger.publish({ error_message:e.inspect })
-            # exchanger.publish({ stack_trace:e.backtrace.join("\n") })
           end
         end
       end
