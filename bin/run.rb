b8bbf450c4fe0afee68806b2a9c8521e69f1ea21
anoiaque <anoiaque@gmail.com>
2013-07-05 15:51:56 +0200

    set we are on vulcain core. Used to know if code is hot loaded in one string via RBMQ message

--- bin/run.rb
+++ bin/run.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
 ENV['DISPLAY'] = ':0' if ENV['DISPLAY'].nil?
+ENV['VULCAIN-CORE'] = '1'
 require File.join(File.dirname(__FILE__), '../lib/core/vulcain')
 Vulcain.spawn_new_worker
